97e14d84fc1b1e407ef102ba6dacd6d0f1ec1484
Luiz paulo de souza fontes junior <82241012+LPfontes@users.noreply.github.com>
2024-08-22 19:48:15 -0300

    Create class_operacoes.py

---
+++ class_operacoes.py
@@ -0,0 +1,21 @@
+class Operacoes:
+    def __init__():
+        return
+
+    def inserir(objeto, tabela):
+        return
+
+    def alterar(objeto, tabela):
+        return
+
+    def pesquisar_por_nome(nome, tabela):
+        return
+
+    def remover(objeto, tabela):
+        return
+
+    def listar_todos(tabela):
+        return
+
+    def exibir_um(objeto):
+        return
